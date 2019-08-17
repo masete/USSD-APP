84ee47aa4fc88462f5123fcb7cee53c312f7fba7
masete <nicholasmasete72@gmail.com>
2019-08-17 18:17:21 +0300

    add flow

--- src/server.js
+++ src/server.js
@@ -22,22 +22,42 @@ app.post('*', (req, res) => {
     res.send(response)
   } else if (text == '1') {
     // Business logic for first level response
-    let response = `CON Choose account information you want to view
+    let response = `CON Choose the period
     1. Daily
     2. Weekly 
     3. Monthly`
     res.send(response)
-  } else if (text == '3') {
-    // Business logic for first level response
-    let response = `END Your phone number is ${phoneNumber}`
+  } else if(text == '3'){
+    let response = `CON choose your tax monthly tax ammount
+    1. 200,000
+    2. 500,000
+    3. 100,000`
     res.send(response)
-  } else if (text == '1*1') {
+  } else if(text == '2'){
+    let response = `CON choose your tax weekly tax ammount
+    1. 5,000
+    2. 15,000
+    3. 20,000`
+    res.send(response)
+  } else if(text == '1'){
+    let response = `CON choose your tax monthly tax ammount
+    1. 500
+    2. 1,000
+    3. 1,500`
+    res.send(response)
+  }
+  //  else if (text == '2') {
     // Business logic for first level response
-    let accountNumber = 'ACC1001'
+    // let response = `END Your phone number is ${phoneNumber}`
+    // res.send(response)
+  // } else if (text == '1*1') {
+    // Business logic for first level response
+    // let accountNumber = 'ACC1001'
     // This is a terminal request. Note how we start the response with END
-    let response = `END Your account number is ${accountNumber}`
-    res.send(response)
-  } else if (text == '1*2') {
+    // let response = `END Your account number is ${accountNumber}`
+    // res.send(response)
+  // } 
+  else if (text == '1*2') {
     // This is a second level response where the user selected 1 in the first instance
     let balance = 'NGN 10,000'
     // This is a terminal request. Note how we start the response with END
